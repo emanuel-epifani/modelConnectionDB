f1a1d5670b5f4951a3ba3b7f729cdc760d52c8f5
Emanuel Epifani <emanuel.epifani.m@gmail.com>
2022-04-21 15:53:16 +0200

    provate tutte le crud (solo update non funziona)

--- src/main/java/Debug.java
+++ src/main/java/Debug.java
@@ -1,13 +1,31 @@
+import modelconnectiondb.model.Studente;
 import modelconnectiondb.repository.StudenteRepository;
 
 public class Debug {
 
 
     public static void main(String[] args) {
+
         //Read
+        System.out.println("\n\n\n\n----------SELECT-----------");
         System.out.println(StudenteRepository.selectStudenti());
 
+        //Insert
+        System.out.println("\n\n\n\n----------INSERT-----------");
+        Studente s1= new Studente("Pippo","Gialli", "Maschio");
+        StudenteRepository.insertStudenti(s1);
+        System.out.println(StudenteRepository.selectStudenti());//x vedere se inserito
 
+        //Delete
+        System.out.println("\n\n\n\n----------DELETE-----------");
+        StudenteRepository.deleteStudente(s1);
+        System.out.println(StudenteRepository.selectStudenti());//x vedere se eliminato
+
+        //Update
+        System.out.println("\n\n\n\n----------UPDATE-----------");
+        Studente s2= new Studente("Zeno","Vignolini", "Maschio");
+        StudenteRepository.updateStudente(s2);
+        System.out.println(StudenteRepository.selectStudenti());
 
 
     }
